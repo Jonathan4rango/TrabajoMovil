49fdf334e3d45d8fc6a3dd993795e2d7d143d9bb
Jonathan Arango <jonathan.arango@utp.edu.co>
2023-02-23 19:06:38 -0500

    Trabajo de clase

---
+++ MOTORSOFT_PROJECT/MOTORSOFT_PROJECT/MOTORSOFT_PROJECT/CalcularGrados.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MOTORSOFT_PROJECT
+{
+    internal class CalcularGrados
+    {
+        CalcularGrados()
+        {
+            double result;
+
+            result = Convert.ToDouble(Console.ReadLine());
+
+           
+            Console.WriteLine(ConvertirGrados(result));
+        }
+
+        double ConvertirGrados(double grados)
+        {
+
+    
+
+            return (grados*1.8+32);
+
+
+
+        }
+    }
+}
---
+++ MOTORSOFT_PROJECT/MOTORSOFT_PROJECT/MOTORSOFT_PROJECT/RetornaPar.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MOTORSOFT_PROJECT
+{
+    internal class RetornaPar
+    {
+
+
+        void llenarArray()
+        {
+            int cont=0;
+            int[] par = new int[50];
+
+            while(cont >= 100)
+            {
+
+
+                if (cont % 2 == 0)
+                {
+                    par[cont] = cont;
+                } 
+                cont++;
+            }
+        }
+
+
+    }
+}
