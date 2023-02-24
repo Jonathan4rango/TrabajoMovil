efc564859828d12ae98ce421ff0a3c1675244071
Jonathan4rango <112779010+Jonathan4rango@users.noreply.github.com>
2023-02-23 19:13:33 -0500

    Add files via upload

---
+++ CalcularSuperficie.cs
@@ -0,0 +1,36 @@
+﻿using MOTORSOFT_PROJECT;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MOTORSOFT_PROJECT
+{
+
+    
+    internal class CalcularSuperficie
+    {
+        CalcularSuperficie()
+
+        {
+            
+            Console.WriteLine(CalcularSuperficiee(7, 8));
+        }
+        int CalcularSuperficiee(int basee, int altura)
+        {
+            int superficie;
+            superficie = basee * altura;
+
+            return superficie;
+
+           
+
+        }
+    }
+
+
+
+  
+
+}
+
+
