c7cbad81759b8dcba0481ba43a0e3d903fd5147c
Jonathan Arango <jonathan.arango@utp.edu.co>
2023-03-09 19:44:51 -0500

    Trabajo login

---
+++ login/login/login/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using login.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace login.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ login/login/login/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using login.Models;
+using login.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace login.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
